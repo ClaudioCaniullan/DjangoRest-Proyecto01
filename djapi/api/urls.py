e2a187884bbc4792ec13694218a667a9322cf0bc
ClaudioCaniullan <claudio.caniullan@usach.cl>
2023-06-29 20:34:00 -0400

    modificar apiviews.py: agregar CategoriaDetalle, modificar SubcategoriaList, modificar crear y modificar respectivas urls

--- djapi/api/urls.py
+++ djapi/api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from api.apiviews import ProductoList, ProductoDetalle, CategoriaList, SubCategoriaList
+from api.apiviews import ProductoList, ProductoDetalle, CategoriaList, SubCategoriaList, CategoriaDetalle
  
 urlpatterns = [
     path('v1/productos/', ProductoList.as_view(),name='producto_list' ),
     path('v1/productos/<int:pk>', ProductoDetalle.as_view(),name='producto_detalle' ),
-    path('v1/categorias/', CategoriaList.as_view(),name='categoria_list' ),
-    path('v1/subcategorias/', SubCategoriaList.as_view(),name='subcategoria_list' )
+    path('v1/categorias/', CategoriaList.as_view(),name='categoria_save' ),
+    #path('v1/subcategorias/', SubCategoriaList.as_view(),name='subcategoria_list' )
+    path('v1/categorias/<int:pk>', CategoriaDetalle.as_view(),name='categoria_detalle' ),
+    path('v1/categorias/<int:pk>/subcategorias/', SubCategoriaList.as_view(),name='sc_list' ),
 ]
